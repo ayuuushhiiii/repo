27311e603b23ad0da07e8eea04e45a28814d8498
Ayushi Saxena <Ayushi.Saxena@insight.com>
2025-08-18 11:25:31 +0530

    commented lines for backend

--- VPC_module/backend.tf
+++ VPC_module/backend.tf
@@ -1,9 +1,9 @@
-terraform {
-  backend "s3" {
-    bucket = "stoirageeee"
-    key = "github/tf.state"
-    region = "us-east-1"
-    encrypt = true
-    dynamodb_table = "tf-state-lock"
-  }
-}
+# terraform {
+#   backend "s3" {
+#     bucket = "stoirageeee"
+#     key = "github/tf.state"
+#     region = "us-east-1"
+#     encrypt = true
+#     dynamodb_table = "tf-state-lock"
+#   }
+# }
