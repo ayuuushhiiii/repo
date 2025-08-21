d0356ecfb5b3037bb5c8c236c4c711917e0d3750
Ayushi Saxena <Ayushi.Saxena@insight.com>
2025-08-22 00:35:07 +0530

    pushing changes for route

--- ROUTETABLE_module/variables.tf
+++ ROUTETABLE_module/variables.tf
@@ -6,18 +6,18 @@ variable "vpc_id" {
 variable "cidr_block" {
   description = "range of CIDR"
   default = ""
-  type = ""
+  type = string
 }
 variable "ig_id" {
   description = "ID of internet gateway"
   default = ""
   type = string
 }
-variable "route_name" {
-  description = "Name of route table"
-  default = ""
-  type = string
-}
+# variable "route_name" {
+#   description = "Name of route table"
+#   default = ""
+#   type = string
+# }
 variable "gateway_id" {
   description = "ID of gateway"
   default = ""
